4a4316a1d7c0410bce6690ae63673b7d63749a69
Ronnypetson <rsronnypetson4@gmail.com>
2017-11-27 19:02:26 -0300

    upload

---
+++ app/src/main/java/com/Config.java
@@ -0,0 +1,9 @@
+package com;//package info.androidhive.camerafileupload;
+
+public class Config {
+    // File upload url (replace the ip with your server address)
+    public static final String FILE_UPLOAD_URL = "http://10.0.0.8:8000";
+
+    // Directory name to store captured images and videos
+    public static final String IMAGE_DIRECTORY_NAME = "skin_imgs";
+}

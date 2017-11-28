05d0a3bbc6315346dcc67a6e18b15b3d63afd0bf
Ronnypetson <rsronnypetson4@gmail.com>
2017-11-28 17:45:50 -0300

    remove video button

--- app/src/main/java/com/Config.java
+++ app/src/main/java/com/Config.java
@@ -2,7 +2,7 @@ package com;//package info.androidhive.camerafileupload;
 
 public class Config {
     // File upload url (replace the ip with your server address)
-    public static String FILE_UPLOAD_URL = "http://10.0.0.108:8000";
+    public static String FILE_UPLOAD_URL = "http://ec2-52-67-221-229.sa-east-1.compute.amazonaws.com:8000";
 
     // Directory name to store captured images and videos
     public static final String IMAGE_DIRECTORY_NAME = "skin_imgs";
